c5bdfff0ea98f751a9b4a1daf87c3c7c66eaaae9
ggunti <ggunty96@gmail.com>
2018-10-27 10:52:15 +0300

    index.js modified to export src/App.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 "react-native": "0.57.4"
 */
 
-import { AppRegistry } from 'react-native';
 import App from './src/App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default App;
